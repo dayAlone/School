1d857d90d646acced150927fd1f3defb7bc4be0d
dayAlone <andrey.slider@gmail.com>
2015-10-11 18:06:41 +0500

    Port

--- server/index.js
+++ server/index.js
@@ -13,4 +13,4 @@ app.keys = [config.secret];
 initMiddlewares(app);
 initControllers(app);
 
-app.listen(process.env.NODE_ENV === 'production' ? 80 : 3000);
+app.listen(process.env.NODE_ENV === 'production' ? ( process.env.PORT ? process.env.PORT : 80 ) : 3000);
